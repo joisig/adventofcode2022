5e67878d5e42b486de158c7a2e01dcb064b37221
Jói Sigurdsson <joi@crankwheel.com>
2022-12-20 17:03:15 +0000

    D18p2 Elixir

--- lib/d18.exs
+++ lib/d18.exs
@@ -42,8 +42,19 @@ defmodule D18 do
   def toward([x,y,z]), do: [x,y-1,z]
   def away([x,y,z]), do: [x,y+1,z]
 
-  def uncovered(map, coords) do
-    1 - Map.get(map, coords, 0)
+  def uncovered(map, [x,y,z] = coords) do
+    {{min_x, min_y, min_z}, {max_x, max_y, max_z}} = Process.get(:min_max_coords)
+    case [x,y,z] do
+      [d,_,_] when d < min_x or d > max_x -> 0
+      [_,d,_] when d < min_y or d > max_y -> 0
+      [_,_,d] when d < min_z or d > max_z -> 0
+      _ ->
+        case Map.get(map, coords) do
+          1 -> 0
+          2 -> 0
+          nil -> 1
+        end
+    end
   end
 
   def uncovered_for_voxel(map, coords) do
@@ -67,12 +78,49 @@ defmodule D18 do
   end
 
   def min_max_coords(map) do
-    [max_x, max_y, max_z] = Enum.reduce(v, [0,0,0], fn {[x,y,z], _}, acc ->
-      [max(x,Enum.at(acc, 0)), max(y, Enum.at(acc,1)), max(z, Enum.att(acc,2))]
+    [max_x, max_y, max_z] = Enum.reduce(map, [1,1,1], fn {[x,y,z], _}, acc ->
+      [max(x,Enum.at(acc, 0)), max(y, Enum.at(acc,1)), max(z, Enum.at(acc,2))]
     end)
-    [min_x, min_y, min_z] = Enum.reduce(v, [0,0,0], fn {[x,y,z], _}, acc ->
-      [min(x,Enum.at(acc, 0)), min(y, Enum.at(acc,1)), min(z, Enum.att(acc,2))]
+    [min_x, min_y, min_z] = Enum.reduce(map, [1,1,1], fn {[x,y,z], _}, acc ->
+      [min(x,Enum.at(acc, 0)), min(y, Enum.at(acc,1)), min(z, Enum.at(acc,2))]
     end)
     {{min_x - 1, min_y - 1, min_z - 1}, {max_x + 1, max_y + 1, max_z + 1}}
+  end
+
+  def fill_empty(map, coords) do
+    case uncovered(map, coords) do
+      0 ->
+        map
+      _ ->
+        map = Map.put(map, coords, 2)
+        map = fill_empty(map, up(coords))
+        map = fill_empty(map, down(coords))
+        map = fill_empty(map, left(coords))
+        map = fill_empty(map, right(coords))
+        map = fill_empty(map, toward(coords))
+        map = fill_empty(map, away(coords))
+        map
+    end
+  end
+
+  def p2() do
+    map = input() |> parse()
+    min_max_coords = {{min_x, min_y, min_z}, {max_x, max_y, max_z}} = min_max_coords(map)
+    Process.put(:min_max_coords, min_max_coords)
+    map = fill_empty(map, [min_x, min_y, min_z])
+    # Any voxel inside the bounding box that is not a 2 ("gas")
+    # is now the surface that we consider.
+    map = Enum.filter(map, fn {_, val} -> val == 2 end) |> Enum.into(%{})
+    no_gas = Enum.reduce(min_x..max_x, %{}, fn x, acc ->
+      Enum.reduce(min_y..max_y, acc, fn y, acc ->
+        Enum.reduce(min_z..max_z, acc, fn z, acc ->
+          case Map.get(map, [x,y,z]) do
+            2 -> Map.put(acc, [x,y,z], 1)
+            _ -> acc
+          end
+        end)
+      end)
+    end)
+    uncovered_for_droplet(no_gas)
   end
 end
