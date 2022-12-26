7c5f67e4cdcbeb7a64b36ef3ca81cad53c050869
Jói Sigurdsson <joi@crankwheel.com>
2022-12-26 12:27:57 +0000

    D12p1 Elixir

--- lib/d12.exs
+++ lib/d12.exs
@@ -66,32 +66,52 @@ defmodule D12 do
 
   def p1() do
     {start, goal, map} = input |> parse
-    walk(goal, start, map)
+    bfs(map, start, goal)
   end
 
   def add_path_if_ok(paths, map, [current|_] = path, next, reached) do
     if next in reached do
       # No point in reaching a point now that was reached using
       # a shorter path already.
+      #IO.inspect "Not adding path; already reached"
       paths
     else
       current_height = Map.get(map, current)
       case Map.get(map, next) do
-        nil -> paths
-        too_high when too_high > current_height + 1 -> paths
+        nil ->
+          #IO.inspect "Not adding path; outside of map"
+          paths
+        too_high when too_high > current_height + 1 ->
+          #IO.inspect "Not adding path; too high"
+          paths
         _height ->
+          #IO.inspect "Adding to path"
           [[next|path]|paths]
       end
     end
   end
 
   def bfs(map, start, goal) do
-    Enum.reduce(1..10, {[[start]], MapSet.new |> MapSet.put(start)}, fn _ix, acc ->
-      bfs_impl(acc, map, goal)
-    end)
+    bfs_impl({[[start]], MapSet.new |> MapSet.put(start)}, map, goal)
   end
 
   def bfs_impl({paths, reached}, map, goal) do
+    #IO.inspect {paths, reached, map, goal}
+    IO.inspect "Length is #{Enum.at(paths, 0) |> Enum.count}, max height is #{map[Enum.at(paths,0) |> Enum.at(0)]}"
+    #IO.inspect Enum.take(paths, 3)
+    {paths, reached} = bfs_step({paths, reached}, map, goal)
+    #IO.inspect Enum.take(paths, 3)
+    highest = Enum.at(paths, 0)
+    #IO.inspect highest
+    if Enum.at(highest, 0) == goal do
+      Enum.count(highest)
+    else
+      paths = Enum.take(paths, 100000)  # Needed a bit of trial and error for cut-off point that worked
+      bfs_impl({paths, reached}, map, goal)
+    end
+  end
+
+  def bfs_step({paths, reached}, map, goal) do
     paths2 = Enum.reduce(paths, [], fn [current|_] = path, acc ->
       acc
       |> add_path_if_ok(map, path, up(current), reached)
