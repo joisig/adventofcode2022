c5dc5d2b790a2491229a3768753a17b7d298b090
Jói Sigurdsson <joi@crankwheel.com>
2022-12-26 12:48:00 +0000

    Fix a bug and do much less work by explicitly doing it as a set-based flood-fill with rules

--- lib/d12.exs
+++ lib/d12.exs
@@ -69,62 +69,49 @@ defmodule D12 do
     bfs(map, start, goal)
   end
 
-  def add_path_if_ok(paths, map, [current|_] = path, next, reached) do
+  def add_path_if_ok(reached, map, current, next) do
     if next in reached do
       # No point in reaching a point now that was reached using
       # a shorter path already.
       #IO.inspect "Not adding path; already reached"
-      paths
+      reached
     else
       current_height = Map.get(map, current)
       case Map.get(map, next) do
         nil ->
           #IO.inspect "Not adding path; outside of map"
-          paths
+          reached
         too_high when too_high > current_height + 1 ->
           #IO.inspect "Not adding path; too high"
-          paths
+          reached
         _height ->
           #IO.inspect "Adding to path"
-          [[next|path]|paths]
+          MapSet.put(reached, next)
       end
     end
   end
 
   def bfs(map, start, goal) do
-    bfs_impl({[[start]], MapSet.new |> MapSet.put(start)}, map, goal)
+    bfs_impl(MapSet.new |> MapSet.put(start), 1, map, goal)
   end
 
-  def bfs_impl({paths, reached}, map, goal) do
-    #IO.inspect {paths, reached, map, goal}
-    IO.inspect "Length is #{Enum.at(paths, 0) |> Enum.count}, max height is #{map[Enum.at(paths,0) |> Enum.at(0)]}"
-    #IO.inspect Enum.take(paths, 3)
-    {paths, reached} = bfs_step({paths, reached}, map, goal)
-    #IO.inspect Enum.take(paths, 3)
-    highest = Enum.at(paths, 0)
-    #IO.inspect highest
-    if Enum.at(highest, 0) == goal do
-      Enum.count(highest)
+  def bfs_impl(reached, iterations, map, goal) do
+    IO.inspect "Length is #{iterations}"
+    reached = bfs_step(reached, map, goal)
+    if goal in reached do
+      iterations
     else
-      paths = Enum.take(paths, 100000)  # Needed a bit of trial and error for cut-off point that worked
-      bfs_impl({paths, reached}, map, goal)
+      bfs_impl(reached, iterations + 1, map, goal)
     end
   end
 
-  def bfs_step({paths, reached}, map, goal) do
-    paths2 = Enum.reduce(paths, [], fn [current|_] = path, acc ->
+  def bfs_step(previously_reached, map, goal) do
+    Enum.reduce(previously_reached, previously_reached, fn current, acc ->
       acc
-      |> add_path_if_ok(map, path, up(current), reached)
-      |> add_path_if_ok(map, path, down(current), reached)
-      |> add_path_if_ok(map, path, left(current), reached)
-      |> add_path_if_ok(map, path, right(current), reached)
+      |> add_path_if_ok(map, current, up(current))
+      |> add_path_if_ok(map, current, down(current))
+      |> add_path_if_ok(map, current, left(current))
+      |> add_path_if_ok(map, current, right(current))
     end)
-    |> Enum.sort(fn [lfirst|_], [rfirst|_] ->
-      map[lfirst] >= map[rfirst]
-    end)
-    reached2 = Enum.reduce(paths2, reached, fn [first|_], acc ->
-      MapSet.put(acc, first)
-    end)
-    {paths2, reached2}
   end
 end
