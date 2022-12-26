7ec1b8815be58017e2c7ba11d16d38c069abcedd
Jói Sigurdsson <joi@crankwheel.com>
2022-12-26 12:56:04 +0000

    D12p2 Elixir

--- lib/d12.exs
+++ lib/d12.exs
@@ -33,43 +33,12 @@ defmodule D12 do
   def left({x,y}), do: {x-1,y}
   def right({x,y}), do: {x+1,y}
 
-  # This is a reverse walk from the goal to the start as that
-  # eliminates possibilities more quickly (I think).
-  def walk(current, goal, map, num_steps \\ 0, prev_height \\ 0, visited \\ [])
-  def walk(current, goal, map, num_steps, prev_height, visited) do
-    if current in visited do
-      999999999999
-    else
-      if current == goal and map[current] >= prev_height - 1 do
-        num_steps
-      else
-        case map[current] do
-          too_low when too_low < prev_height - 1 ->
-            999999999999
-          nil ->
-            999999999999
-          height ->
-            new_visited = [current|visited]
-            new_steps = num_steps + 1
-            [
-              walk(up(current), goal, map, new_steps, height, new_visited),
-              walk(down(current), goal, map, new_steps, height, new_visited),
-              walk(left(current), goal, map, new_steps, height, new_visited),
-              walk(right(current), goal, map, new_steps, height, new_visited),
-            ]
-            |> Enum.sort()
-            |> Enum.at(0)
-        end
-      end
-    end
-  end
-
   def p1() do
     {start, goal, map} = input |> parse
     bfs(map, start, goal)
   end
 
-  def add_path_if_ok(reached, map, current, next) do
+  def add_path_if_ok(reached, map, current, next, :walk_up) do
     if next in reached do
       # No point in reaching a point now that was reached using
       # a shorter path already.
@@ -91,13 +60,35 @@ defmodule D12 do
     end
   end
 
+  def add_path_if_ok(reached, map, current, next, :walk_down) do
+    if next in reached do
+      # No point in reaching a point now that was reached using
+      # a shorter path already.
+      #IO.inspect "Not adding path; already reached"
+      reached
+    else
+      current_height = Map.get(map, current)
+      case Map.get(map, next) do
+        nil ->
+          #IO.inspect "Not adding path; outside of map"
+          reached
+        too_low when too_low < current_height - 1 ->
+          #IO.inspect "Not adding path; too high"
+          reached
+        _height ->
+          #IO.inspect "Adding to path"
+          MapSet.put(reached, next)
+      end
+    end
+  end
+
   def bfs(map, start, goal) do
     bfs_impl(MapSet.new |> MapSet.put(start), 1, map, goal)
   end
 
   def bfs_impl(reached, iterations, map, goal) do
     IO.inspect "Length is #{iterations}"
-    reached = bfs_step(reached, map, goal)
+    reached = bfs_step(reached, map, goal, :walk_up)
     if goal in reached do
       iterations
     else
@@ -105,13 +96,29 @@ defmodule D12 do
     end
   end
 
-  def bfs_step(previously_reached, map, goal) do
+  def bfs_step(previously_reached, map, _goal, walk_direction) do
     Enum.reduce(previously_reached, previously_reached, fn current, acc ->
       acc
-      |> add_path_if_ok(map, current, up(current))
-      |> add_path_if_ok(map, current, down(current))
-      |> add_path_if_ok(map, current, left(current))
-      |> add_path_if_ok(map, current, right(current))
+      |> add_path_if_ok(map, current, up(current), walk_direction)
+      |> add_path_if_ok(map, current, down(current), walk_direction)
+      |> add_path_if_ok(map, current, left(current), walk_direction)
+      |> add_path_if_ok(map, current, right(current), walk_direction)
     end)
+  end
+
+  def bfs_impl_goal_height(reached, iterations, map, goal_height) do
+    IO.inspect "Length is #{iterations}"
+    reached = bfs_step(reached, map, 0, :walk_down)
+    reached_at_goal_height = Enum.filter(reached, fn coord -> map[coord] == goal_height end)
+    if [] != reached_at_goal_height do
+      iterations
+    else
+      bfs_impl_goal_height(reached, iterations + 1, map, goal_height)
+    end
+  end
+
+  def p2() do
+    {start, goal, map} = input |> parse
+    bfs_impl_goal_height(MapSet.new |> MapSet.put(goal), 1, map, 0)
   end
 end
